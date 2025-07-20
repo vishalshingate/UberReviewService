b524d6a2b6aa0214f45c22f8912667545e5cec0f
vishalshingate <vishalshingate26@gmail.com>
2025-07-20 17:24:56 +0530

    Updated code created more colunns

--- src/main/java/com/example/uberreviewservice/models/Review.java
+++ src/main/java/com/example/uberreviewservice/models/Review.java
@@ -1,10 +1,38 @@
 package com.example.uberreviewservice.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "bookingreview")
 public class Review {
     @Id// this annotation makes the is property a primary key of our table
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(nullable = false)
+    String content;
+
+    String rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp // this annotation tells spring about the handle when once created
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate // only handle when something updated at the row
+    Date updatedAt;
+
 }
